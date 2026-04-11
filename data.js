5d2ed8ef909269e2c8969db205b35cf6fb776739
TitaniumMC-LUNX <denizeymen3535krl@gmail.com>
2026-04-11 14:43:25 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const GAMES_DATA = [
   {
-    "icon": "🩹",
-    "name": "Liste yakında",
-    "desc": "yani birazdan eklenicek sistemler kuruluyor",
-    "status": "active"
+    icon: "🩹",
+    name: "Liste yakında",
+    desc: "yani birazdan eklenicek sistemler kuruluyor",
+    status: "active"
   }
 ];
 
 const UPDATES_DATA = [  
   {
-    "version": "[2]",
-    "date": "11 Nis 2026",
-    "title": "Websitemiz GÜNCELLENDI!!!!!",
-    "desc": "artık daha şık...",
-    "tags": ["new"]
-  };
+    version: "[2]",
+    date: "11 Nis 2026",
+    title: "Websitemiz GÜNCELLENDI!!!!!",
+    desc: "artık daha şık...",
+    tags: ["new"]
+  },
   {
-    "version": "[1]",
-    "date": "03 Nis 2026",
-    "title": "Websitemiz açıldı!!!",
-    "desc": "LUNX - eymen sayesinde [:)]",
-    "tags": ["old"]
+    version: "[1]",
+    date: "03 Nis 2026",
+    title: "Websitemiz açıldı!!!",
+    desc: "LUNX - eymen sayesinde [:)]",
+    tags: ["old"]
   }
 ];
